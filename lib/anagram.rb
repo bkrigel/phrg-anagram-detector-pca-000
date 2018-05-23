f362ed223c3da7868f1a114a974133447381488f
bkrigel <benjamin.krigel@gmail.com>
2018-05-23 16:16:36 -0400

    Passed all tests on one try

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    anagram_matches = []
+    words_array.each do |ana_word|
+      split_word = ana_word.split("")
+      split_original_word = @word.split("")
+      if split_word.sort == split_original_word.sort
+        anagram_matches << ana_word
+      end
+    end
+    anagram_matches
+  end
+
+end
